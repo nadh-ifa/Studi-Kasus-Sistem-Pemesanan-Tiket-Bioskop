9c9f07d25f297ae3326ef260279a444d0e5c2b62
nadh-ifa <nadhifafitriyaah@gmail.com>
2025-02-24 21:16:26 +0700

    Create Penonton.java
    Kelas penonton

---
+++ Penonton.java
@@ -0,0 +1,11 @@
+public class Penonton {
+    String nama; // Variabel untuk menyimpan nama penonton
+
+    void setNama(String myNama) { // Method untuk mengatur nama penonton
+        nama = myNama;
+    }
+
+    void displayInfo() { // Method untuk menampilkan informasi penonton
+        System.out.println("Nama Penonton: " + nama);
+    }
+}
